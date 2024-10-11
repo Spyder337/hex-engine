a3656c03a340e8375c62a82d17623cce6a6ea63c
Owsley Wood <owsley.wood@gmail.com>
2024-10-11 07:45:27 -0700

    Updated: 11-10-2024 7:45:27

---
+++ Grids/Neighbors.cs
@@ -0,0 +1,34 @@
+﻿using Grids.Positions;
+using System.Collections.Generic;
+
+namespace Grids.Neighbors;
+
+public static class Cube
+{
+    /// <summary>
+    /// Directions in the cube coordinate space.
+    /// 0 = North, 1 = North-East, 2 = South-East, 3 = South,
+    /// 4 = South-West, 5 = North-West.
+    /// </summary>
+    public const CubePos[] CUBE_DIRS = new CubePos[
+        //  Flat top directions.
+        //  North
+        new CubePos(0, -1, 1),
+        //  North-East
+        new CubePos(1, -1, 0),
+        //  South-East
+        new CubePos(1, 0, -1),
+        //  South
+        new CubePos(0, 1, -1),
+        //  South-West
+        new CubePos(-1, 1, 0),
+        //  North-West
+        new CubePos(-1, 0, 1)
+    ];
+
+    public static CubePos CubeDirection(int dir)
+    {
+        return CUBE_DIRS[dir];
+    }
+}
+
--- hex-grid/Program.cs
+++ hex-grid/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Grids.Positions;
+using Grids.Neighbors;
 
 var pos = new CubePos(2, 2,-4);
-var pos2 = OffsetPos.FromCube(pos, OffsetType.OddR);
+var pos2 = new CubePos(0, -1, 1);
+
+var pos3 = pos + pos2;
 
 Console.WriteLine($"X: {pos2.Q}, Y: {pos2.R}");
